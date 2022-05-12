e49f79024be2dce87ec96360b650e705b17ebd23
Ariel Ricardo Suarez Baez <suarezariel86@gmail.com>
2022-05-12 18:05:05 -0300

    avances con las validaciones del input y mensajes de error personalizados

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-let input;
+const input = document.querySelector("#converter-box");
 function inputValue() {
-  input = document.querySelector("#converter-box").value;
-  input.placeholder = "ingrese texto";
-  return input;
+  return input.value;
 }
 
 const btnEncrypt = document.querySelector("#encrypt-btn");
 const btnDecrypt = document.querySelector("#decrypt-btn");
 const btnCopy = document.querySelector("#copy-btn");
 const result = document.querySelector("#result");
+const errorText = document.querySelector("#error-text");
+errorText.classList.add("errors");
+const regExpMayus = /[A-Z]/g;
+const regExpCaract = /\W/g;
 
 function writeResult(text) {
   result.textContent = text;
+}
+function clearInput() {
+  input.value = "";
 }
 const encryptRules = { e: "enter", i: "imes", a: "ai", o: "ober", u: "ufat" };
 function findKey(value) {
@@ -22,31 +27,58 @@ function findKey(value) {
 }
 
 function encrypt() {
-  inputValue();
-
-  const newText = input
+  const newText = input.value
     .replaceAll("e", encryptRules.e)
     .replaceAll("i", encryptRules.i)
     .replaceAll("a", encryptRules.a)
     .replaceAll("o", encryptRules.o)
     .replaceAll("u", encryptRules.u);
   writeResult(newText);
+  clearInput();
 }
 function decrypt() {
-  inputValue();
-  const newText = input
+  const newText = input.value
     .replaceAll("enter", findKey("enter"))
     .replaceAll("imes", findKey("imes"))
     .replaceAll("ai", findKey("ai"))
     .replaceAll("ober", findKey("ober"))
     .replaceAll("ufat", findKey("ufat"));
   writeResult(newText);
+  clearInput();
 }
-btnCopy.addEventListener("click", function copy() {
+function copy() {
   result.select();
   result.setSelectionRange(0, 9999);
   document.execCommand("copy");
-});
+}
 
+function printError1(letter, caracter) {
+  errorText.textContent = `Error: Ingresaste las siguientes letras mayuscula y caracteres: (${letter}),(${caracter})`;
+}
+function printError2(letter) {
+  errorText.textContent = `Error: Ingresaste las siguientes letras en mayuscula: ${letter}`;
+}
+function printError3(caracter) {
+  errorText.textContent = `Error: Ingresaste los siguientes caracteres: ${caracter}`;
+}
+
+function changes() {
+  const testMayus = regExpMayus.test(input.value);
+  const testCaracter = regExpMayus.test(input.value);
+  const letterMayus = input.value.match(regExpMayus);
+  const caracter = input.value.match(regExpCaract);
+  if (testMayus && caracter !== null) {
+    printError1(letterMayus, caracter);
+  } else if (testMayus) {
+    printError2(letterMayus);
+  } else if (caracter !== null) {
+    printError3(caracter);
+  } else {
+    errorText.textContent = "";
+  }
+}
+
+input.oninput = changes;
 btnEncrypt.onclick = encrypt;
 btnDecrypt.onclick = decrypt;
+btnCopy.onclick = copy;
